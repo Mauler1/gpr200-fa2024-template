9a5c186d492ec6017fc84b316c30703eaa30fae4
Abrahm Routhier <110110992+Mauler1@users.noreply.github.com>
2024-09-15 00:14:28 -0400

    Shader header file and part 1 of tutorial implementation in cpp file

--- core/arout/shader.cpp
+++ core/arout/shader.cpp
@@ -1,4 +1,39 @@
 #include "shader.h"
+
 namespace arout {
-	//smile :)
+	class Shader {
+		Shader(const char* vertexPath, const char* fragmentPath) {
+			std::string vertexCode;
+			std::string fragmentCode;
+			std::ifstream vShaderFile;
+			std::ifstream fShaderFile;
+
+			vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+			fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+			try {
+
+				//open the files
+				vShaderFile.open(vertexPath);
+				fShaderFile.open(fragmentPath);
+				std::stringstream vShaderStream, fShaderStream;
+
+				//read file buffer contents into streams
+				vShaderStream << vShaderFile.rdbuf();
+				fShaderStream << fShaderFile.rdbuf();
+
+				//close files
+				vShaderFile.close();
+				fShaderFile.close();
+
+				//convert stream into string
+				vertexCode = vShaderStream.str();
+				fragmentCode = fShaderStream.str();
+			}
+			catch (std::ifstream::failure e) {
+				std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+			}
+			const char* vShaderCode = vertexCode.c_str();
+			const char* fShaderCode = fragmentCode.c_str();
+		}
+	};
 }
--- core/arout/shader.h
+++ core/arout/shader.h
@@ -1,9 +1,25 @@
 #pragma once
 
 #include "../ew/external/glad.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 namespace arout {
 	class Shader {
-		//smile :)
+		//program ID
+		unsigned int ID;
+
+		// constructor
+		Shader(const char* vertexPath, const char* fragmentPath);
+
+		//use/activate shader
+		void use();
+
+		//utility uniform functions
+		void setBool(const std::string &name, bool value) const;
+		void setInt(const std::string& name, int value) const;
+		void setFloat(const std::string& name, float value) const;
 	};
 }
