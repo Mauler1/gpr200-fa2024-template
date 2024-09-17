3890152ac8990058299c2276a858119001316155
Abrahm Routhier <110110992+Mauler1@users.noreply.github.com>
2024-09-17 11:17:11 -0400

    Assignment 2 make

---
+++ assignments/assignment_2/main.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+#include <ew/external/glad.h>
+#include <ew/ewMath/ewMath.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <arout/shader.cpp>
+
+const int SCREEN_WIDTH = 1080;
+const int SCREEN_HEIGHT = 720;
+
+float vertices[] = {
+	//X		Y		Z	R		G	B		A
+	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+	 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
+};
+const char* vertexShaderSource = "assets/vertexShader.vert";
+
+const char* fragmentShaderSource = "assets/fragmentShader.frag";
+
+
+
+int main() {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return 1;
+	}
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return 1;
+	}
+	//Initialization goes here!
+
+	arout::Shader helloTriangleShader(vertexShaderSource, fragmentShaderSource);
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//	XYZ position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// RGBA color
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(sizeof(float)*3));
+	glEnableVertexAttribArray(1);
+
+
+	//Render loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
+
+		//Clear framebuffer
+		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		helloTriangleShader.use();
+
+		//set time uniform
+		helloTriangleShader.setFloat("uTime", time);
+
+		glBindVertexArray(VAO);
+
+		//draw call
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//Drawing happens here!
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
